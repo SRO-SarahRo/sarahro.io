439a759520a3bfd1727849efccad09b55cda2838
Imo999 <rcajina.pro@gmail.com>
2021-05-24 19:29:26 +0200

    add page SocialNetwork

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -6,9 +6,9 @@ const SocialNetwork = () => {
   return (
     <div className="social-network">
       <ul className="logo-socialNetwork">
-        <a href="https://github.com/SRO-SarahRo" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-github" />
-        <a href="https://twitter.com/SarahroSro" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-twitter" />
-        <a href="https://www.linkedin.com/company/sarahro" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-linkedin" />
+        <a href="https://github.com/SRO-SarahRo" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-github">{null}</a>
+        <a href="https://twitter.com/SarahroSro" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-twitter">{null}</a>
+        <a href="https://www.linkedin.com/company/sarahro" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-linkedin">{null}</a>
       </ul>
     </div >
   )
