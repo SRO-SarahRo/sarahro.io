3e00c407be18f43615c9590e330692eaeaac2915
Imo999 <rcajina.pro@gmail.com>
2021-05-24 22:54:04 +0200

    Update SocialNetwork : add Twitter link

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -7,7 +7,7 @@ const SocialNetwork = () => {
     <div className="social-network">
       <ul className="logo-socialNetwork">
         <a href="https://github.com/SRO-SarahRo" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-github">{null}</a>
-        <a href="https://twitter.com/SarahroSro" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-twitter">{null}</a>
+        <a href="https://twitter.com/Sarahro_Sro" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-twitter">{null}</a>
         <a href="https://www.linkedin.com/company/sarahro" target="_blank" rel="noopener noreferrer" className="hover" class="fab fa-linkedin">{null}</a>
       </ul>
     </div >
