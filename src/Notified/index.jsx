0d4a1b0568a0ae347958c669aab98a462e74bb94
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-07-26 17:11:20 +0200

    Add get Notified

--- src/Notified/index.jsx
+++ src/Notified/index.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import { Container, FormWrap, Icon, FormContent, Form, FormInput, FormH1, FormLabel, FormButton } from './NotifiedElements'
+import React, { useState } from "react"
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+import { Container, FormWrap, Icon, FormContent, Form, FormInput, FormH1, FormButton, Text } from './NotifiedElements'
+require('dotenv').config();
 
 const Notified = () => {
+
+  const url = "https://sarahro.us6.list-manage.com/subscribe/post?u=dbd591405706d15cebc567ffb&amp;id=7acefdd6ad"
+  const [email, setEmail] = useState("")
+
   return (
     <>
       <Container>
         <FormWrap>
           <Icon to='/'>SarahRO</Icon>
-          <FormContent>
-            <Form action='#'>
-              <FormH1>GET NOTIFIED WHEN WE GO LIVE</FormH1>
-              <FormLabel htmlFor='for'>Email Adress..</FormLabel>
-                <FormInput htmlFor='email' required />
-              <FormButton type='submit'>Get Notified</FormButton>
-            </Form>
-          </FormContent>
+         <MailchimpSubscribe
+            url={url}
+            render={({ subscribe, status, message }) => <>
+              <FormContent>
+                <Form onSubmit={e => { subscribe({ EMAIL: email }); e.preventDefault() }}>
+                  <FormH1>GET NOTIFIED WHEN WE GO LIVE</FormH1>
+                  <FormInput htmlFor='email' placeholder="Enter your email..." required onChange={e => setEmail(e.target.value)}/>
+                  <FormButton type='submit'><span className={status === "sending" ? "" : ""}>Get Notified</span></FormButton>
+                  { ["sending", "error", "success"].includes(status) ? <Text><span >
+                    {status === "sending" && <div>Please wait...</div>}
+                    {status === "error" && <div dangerouslySetInnerHTML={{__html: message}}/>}
+                    {status === "success" && <div>Thank you for signing up!</div>}
+                    </span></Text> : null }
+                </Form>
+              </FormContent>
+          </>} />
         </FormWrap>
       </Container>
     </>
