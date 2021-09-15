b0166a0bc778e82291d6b99bf9b20d6b01d611c7
Imo999 <rcajina.pro@gmail.com>
2021-09-15 08:37:04 +0200

    fixed link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,34 +12,19 @@ const Footer = () => {
             <WebsiteRights>SarahRO - SRO © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
             <SocialIcons>
               <SocialIconLink href='https://github.com/SRO-SarahRo' target='_blank' arial-label='Github'>
-                <FaGithub/>
+                <FaGithub />
               </SocialIconLink>
-               <SocialIconLink href='https://t.me/sarahro0' target='_blank' arial-label='Telegram'>
-                <FaTelegram/>
+              <SocialIconLink href='https://twitter.com/Sarahro_Sro' target='_blank' arial-label='Twitter'>
+                <FaTwitter />
               </SocialIconLink>
-              <SocialIconLink href='https://discord.gg/HuQuaWVe' target='_blank' arial-label='Discord'>
-                <FaDiscord/>
+              <SocialIconLink href='https://medium.com/@sarahro_sro' target='_blank' arial-label='Medium'>
+                <FaMedium />
               </SocialIconLink>
-                <SocialIconLink href='https://twitter.com/Sarahro_Sro' target='_blank'  arial-label='Twitter'>
-                <FaTwitter/>
+              <SocialIconLink href='https://t.me/sarahro0' target='_blank' arial-label='Telegram'>
+                <FaTelegram />
               </SocialIconLink>
-              <SocialIconLink href='https://medium.com/@sarahro_sro' target='_blank'  arial-label='Medium'>
-                <FaMedium/>
-              </SocialIconLink>
-              <SocialIconLink href='https://www.linkedin.com/company/sarahro' target='_blank'  arial-label='Linkedin'>
-                <FaLinkedin/>
-              </SocialIconLink>
-              <SocialIconLink href='https://www.instagram.com/sarahro_sro/' target='_blank' arial-label='Instagram'>
-                <FaInstagram/>
-              </SocialIconLink>
-              <SocialIconLink href='https://www.youtube.com/channel/UCSN2ylOtQZwPGmd80jaA0rg' target='_blank'  arial-label='Youtube'>
-                <FaYoutube />
-              </SocialIconLink>
-                 <SocialIconLink href='https://www.facebook.com/SarahRo.SRO' target='_blank'  arial-label='Twitch'>
-                <FaTwitch />
-              </SocialIconLink>
-               <SocialIconLink href='https://www.twitch.tv/sarahro_sro/' target='_blank'  arial-label='Facebook'>
-                <FaFacebook />
+              <SocialIconLink href='' target='_blank' arial-label='Discord'>
+                <FaDiscord />
               </SocialIconLink>
             </SocialIcons>
           </SocialMediaWrap>
