0067d109d7ddb615e2a2c1d7a4fbb4421081221e
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2023-09-04 14:17:59 +0200

    Update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { FaTwitter, FaGithub, FaTelegram } from 'react-icons/fa'
-import { FooterContainer, FooterWrap, SocialIconLink, SocialIcons, SocialMedia, SocialMediaWrap, WebsiteRights } from './FooterElements'
+import { FooterContainer, FooterWrap, SocialMedia, SocialMediaWrap, WebsiteRights } from './FooterElements'
 
 // Add FaDiscord, FaMedium from 'react-icons/fa'
 
@@ -12,23 +11,6 @@ const Footer = () => {
         <SocialMedia>
           <SocialMediaWrap>
             <WebsiteRights>SarahRO - SRO © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
-            <SocialIcons>
-              {/* <SocialIconLink href='https://github.com/SRO-SarahRo' target='_blank' arial-label='Github'>
-                <FaGithub />
-              </SocialIconLink>
-              <SocialIconLink href='https://twitter.com/Sarahro_Sro' target='_blank' arial-label='Twitter'>
-                <FaTwitter />
-              </SocialIconLink> */}
-              {/* <SocialIconLink href='https://medium.com/@sarahro_sro' target='_blank' arial-label='Medium'>
-                <FaMedium />
-              </SocialIconLink> */}
-              {/* <SocialIconLink href='https://t.me/sarahro0' target='_blank' arial-label='Telegram'>
-                <FaTelegram />
-              </SocialIconLink> */}
-              {/* <SocialIconLink href='' target='_blank' arial-label='Discord'>
-                <FaDiscord />
-              </SocialIconLink> */}
-            </SocialIcons>
           </SocialMediaWrap>
         </SocialMedia>
       </FooterWrap>
